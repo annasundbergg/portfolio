c83633ca1c4c1fb23f83818cc1351325af3f8d5e
Anna S <anna.sundberg@geshdo.com>
2024-05-02 18:40:38 +0200

    test Image unoptimized

--- src/app/cv/components/CvHeader.tsx
+++ src/app/cv/components/CvHeader.tsx
@@ -16,6 +16,7 @@ export const CVHeader = () => {
           Anna Sundberg
         </h1>
         <Image
+          unoptimized
           className="rounded-[500px] md:w-[400px] lg:w-[400px] xl:w-[400px] 2xl:w-1/2 "
           src={"/annasundberg.jpg"}
           width={600}
