979519824741786d8f864fa957bfdac2518bce7d
Anna S <anna.sundberg@geshdo.com>
2024-05-21 11:28:00 +0200

    change src of photo in CVHeader

--- src/app/cv/components/CvHeader.tsx
+++ src/app/cv/components/CvHeader.tsx
@@ -1,7 +1,6 @@
 import { fontTitanOne } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
-import profilePic from "../../../../public/annasundberg.jpg";
 
 export const CVHeader = () => {
   return (
@@ -17,7 +16,7 @@ export const CVHeader = () => {
         </h1>
         <Image
           className="rounded-[500px] sm:w-[100px] md:w-[400px] lg:w-[400px] xl:w-[400px] 2xl:w-1/2 "
-          src={profilePic}
+          src={"/annasundberg.jpg"}
           width={600}
           height={600}
           alt="Photo of Anna"
