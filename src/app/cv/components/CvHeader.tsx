c9d48d5ff00c1f5fa924a0a6197da93424dd497a
Anna S <anna.sundberg@geshdo.com>
2024-05-21 11:22:52 +0200

    trying to fix next image not showing in production on cv page

--- src/app/cv/components/CvHeader.tsx
+++ src/app/cv/components/CvHeader.tsx
@@ -1,7 +1,7 @@
 import { fontTitanOne } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
-
 import Image from "next/image";
+import profilePic from "../../../../public/annasundberg.jpg";
 
 export const CVHeader = () => {
   return (
@@ -17,7 +17,7 @@ export const CVHeader = () => {
         </h1>
         <Image
           className="rounded-[500px] sm:w-[100px] md:w-[400px] lg:w-[400px] xl:w-[400px] 2xl:w-1/2 "
-          src={"/annasundberg.jpg"}
+          src={profilePic}
           width={600}
           height={600}
           alt="Photo of Anna"
