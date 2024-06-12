085e4b7286e5c69354a5a2f3bb25a6b567a80628
Anna S <anna.sundberg@geshdo.com>
2024-06-12 17:39:49 +0200

    edit project position in projectList

--- src/app/projects/components/ProjectList.tsx
+++ src/app/projects/components/ProjectList.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Project } from "@/types/project";
 import Image from "next/image";
 import Link from "next/link";
+// import "../../styles/link-on-hover.css";
 
 export default function ProjectList({
   title,
@@ -20,8 +21,8 @@ export default function ProjectList({
       <div className="flex flex-col">
         <div className="sm:hidden md:block md:h-2 bg-[#D55353] rounded"></div>
 
-        <div className="flex sm:flex-col md:flex-row justify-center items-center">
-          <div className="flex flex-col justify-center items-center md:w-6/7 2xl:w-10/12">
+        <div className="flex sm:flex-col md:flex-row items-center justify-center">
+          <div className="flex flex-col justify-center md:w-6/7 2xl:w-10/12">
             {/* TEXT CONTAINER  */}
             <div className="px-4 sm:px-4 md:px-8 lg:px-12 xl:px-14 flex flex-col gap-20 md:gap-24 lg:gap-32 xl:gap-48">
               {/* PROJECT INFO CONTAINER  */}
@@ -64,7 +65,7 @@ export default function ProjectList({
                       fontUbuntu.variable
                     )}
                   >
-                    <Link href={link}>Visit the app</Link>
+                    {/* <Link href={link}>Visit the app</Link> */}
                     {github && <Link href={github}>Visit the github repo</Link>}
                   </div>
                 </div>
@@ -72,16 +73,26 @@ export default function ProjectList({
             </div>
           </div>
           {/* PHOTO CONTAINER  */}
-          <div className="mt-14">
+          {/* dessa är kvar när man klickar på menyn om jag har position relative på image container, behöver ha minus z-index men då funkar inte hover effekten*/}
+
+          <div className="image-container mt-14">
             <Card>
               <CardContent className="flex items-center justify-center p-6">
                 {image && (
-                  <Image
-                    src={image.src}
-                    alt={image.alt}
-                    width={image.width}
-                    height={image.height}
-                  ></Image>
+                  <>
+                    <Image
+                      className="project-img opacity-100 transition-opacity duration-500 ease-in-out"
+                      src={image.src}
+                      alt={image.alt}
+                      width={image.width}
+                      height={image.height}
+                    ></Image>
+                    {/* <div className="absolute text-gallery flex top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity duration-500 ease-in-out">
+                      <Link className="text-gray-600" href={link}>
+                        Visit the app
+                      </Link>
+                    </div> */}
+                  </>
                 )}
               </CardContent>
             </Card>
