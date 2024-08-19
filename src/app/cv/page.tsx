71e0ad9c443035b8f7e5c687f096c57c8e0abffa
Anna S <anna.sundberg@geshdo.com>
2024-08-19 15:12:53 +0200

    testing height CV page

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -8,7 +8,7 @@ import { Bio } from "./components/Bio";
 export default function CVpage() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen sm:w-screen sm:overflow-hidden">
+      <div className="flex justify-center items-center h-full sm:w-screen sm:overflow-hidden">
         <div className="flex flex-col justify-center items-center sm:w-screen md:w-screen 2xl:w-10/12">
           <CVHeader />
           <div className="px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 flex flex-col sm:gap-10 md:gap-24 lg:gap-32 xl:gap-32">
