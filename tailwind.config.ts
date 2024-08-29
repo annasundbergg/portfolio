62aa86272741ccab8deff83d94aa7f7127e233d5
Anna S <anna.sundberg@geshdo.com>
2024-08-29 14:06:55 +0200

    edit screens in config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,29 +11,13 @@ const config = {
   prefix: "",
   theme: {
     screens: {
-      'sm': '390px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '640px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+      sm: "320px",
+      md: "640px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
 
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     fontFamily: {
       sans: [`var(--font-sans)`],
       mono: [`var(--font-mono)`],
